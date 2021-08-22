fde6de334c660c9ccaa1b39654c6e9092151b5c4
Oleksii Patsiuk <patsyuck@ukr.net>
2021-08-22 11:49:59 +0300

    Переніс до index.js функції для роботи з контактами

--- model/index.js
+++ model/index.js
@@ -1,20 +1,82 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
+const path = require('path')
+const contactsPath = path.join(__dirname, 'contacts.json')
 
-const listContacts = async () => {}
+async function readData() {
+  const data = await fs.readFile(contactsPath)
+  const result = JSON.parse(data)
+  return result
+}
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  try {
+    const result = await readData()
+    console.table(result)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  try {
+    const result = await readData()
+    const contact = result.find(item => item.id === contactId)
+    if (!contact) {
+      throw new Error(`Contact with id=${contactId} not found!`)
+    }
+    console.log(contact)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
-const addContact = async (body) => {}
+async function removeContact(contactId) {
+  try {
+    const result = await readData()
+    const idx = result.findIndex(item => item.id === contactId)
+    if (idx === -1) {
+      throw new Error(`Contact with id=${contactId} not found!`)
+    }
+    const contact = result[idx]
+    const contacts = result.filter(item => item.id !== contactId)
+    const newContacts = JSON.stringify(contacts)
+    await fs.writeFile(contactsPath, newContacts)
+    console.log('One contact removed:')
+    console.log(contact)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+// const addContact = async (body) => {}
+async function addContact(name, email, phone) {
+  try {
+    const result = await readData()
+    const idx = result[result.length - 1].id + 1
+    const newContact = {
+      id: idx,
+      name: name,
+      email: email,
+      phone: phone
+    }
+    result[result.length] = newContact
+    const newContacts = JSON.stringify(result)
+    await fs.writeFile(contactsPath, newContacts)
+    console.log('New contact added:')
+    console.log(newContact)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+// const updateContact = async (contactId, body) => { }
+async function updateContact(contactId, name, email, phone) {}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContact
 }
