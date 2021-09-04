57b2e3ef437d98b0c50c551a4698b35a6bdf9e8c
Oleksii Patsiuk <patsyuck@ukr.net>
2021-09-04 11:10:42 +0300

    Додав асинхронну обгортку для обробки помилок

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,103 +3,96 @@ const router = express.Router()
 const contacts = require('../../model')
 const { contactSchema, favoriteSchema } = require('../../validation')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const listContacts = await contacts.listContacts()
-    res.json(listContacts)
-  } catch (error) {
-    next(error)
+const asyncWrapper = (controller) => {
+  return async (req, res, next) => {
+    try {
+      await controller(req, res, next)
+    } catch (error) {
+      next(error)
+    }
   }
-})
+}
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const contact = await contacts.getContactById(contactId)
-    if (!contact) {
-      res.status(404).json({
-        message: 'Not Found'
-      })
-    }
-    res.json(contact)
-  } catch (error) {
-    next(error)
-  }
-})
+const getAllContacts = async (req, res, next) => {
+  const listContacts = await contacts.listContacts()
+  res.json(listContacts)
+}
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body)
-    if (error) {
-      res.status(400).json({
-        message: 'missing required name field'
-      })
-      return
-    }
-    const newContact = await contacts.addContact(req.body)
-    res.status(201).json(newContact)
-  } catch (error) {
-    next(error)
+const getOneContact = async (req, res, next) => {
+  const { contactId } = req.params
+  const contact = await contacts.getContactById(contactId)
+  if (!contact) {
+    res.status(404).json({
+      message: 'Not Found'
+    })
   }
-})
+  res.json(contact)
+}
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body)
-    if (error) {
-      res.status(400).json({
-        message: 'missing fields'
-      })
-      return
-    }
-    const { contactId } = req.params
-    const updatedContact = await contacts.updateContact(contactId, req.body)
-    if (!updatedContact) {
-      res.status(404).json({
-        message: 'Not Found'
-      })
-    }
-    res.json(updatedContact)
-  } catch (error) {
-    next(error)
+const addOneContact = async (req, res, next) => {
+  const { error } = contactSchema.validate(req.body)
+  if (error) {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+    return
   }
-})
+  const newContact = await contacts.addContact(req.body)
+  res.status(201).json(newContact)
+}
 
-router.patch('/:contactId/favorite', async (req, res, next) => {
-  try {
-    const { error } = favoriteSchema.validate(req.body)
-    if (error) {
-      res.status(400).json({
-        message: 'missing field favorite'
-      })
-      return
-    }
-    const { contactId } = req.params
-    const updatedContact = await contacts.updateStatusContact(contactId, req.body)
-    if (!updatedContact) {
-      res.status(404).json({
-        message: 'Not Found'
-      })
-    }
-    res.json(updatedContact)
-  } catch (error) {
-    next(error)
+const updateOneContact = async (req, res, next) => {
+  const { error } = contactSchema.validate(req.body)
+  if (error) {
+    res.status(400).json({
+      message: 'missing fields'
+    })
+    return
   }
-})
+  const { contactId } = req.params
+  const updatedContact = await contacts.updateContact(contactId, req.body)
+  if (!updatedContact) {
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  }
+  res.json(updatedContact)
+}
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const deletedContact = await contacts.removeContact(contactId)
-    if (!deletedContact) {
-      res.status(404).json({
-        message: 'Not Found'
-      })
-    }
-    res.json(deletedContact)
-  } catch (error) {
-    next(error)
+const updateOneStatus = async (req, res, next) => {
+  const { error } = favoriteSchema.validate(req.body)
+  if (error) {
+    res.status(400).json({
+      message: 'missing field favorite'
+    })
+    return
   }
-})
+  const { contactId } = req.params
+  const updatedContact = await contacts.updateStatusContact(contactId, req.body)
+  if (!updatedContact) {
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  }
+  res.json(updatedContact)
+}
+
+const deleteOneContact = async (req, res, next) => {
+  const { contactId } = req.params
+  const deletedContact = await contacts.removeContact(contactId)
+  if (!deletedContact) {
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  }
+  res.json(deletedContact)
+}
+
+router.get('/', asyncWrapper(getAllContacts))
+router.get('/:contactId', asyncWrapper(getOneContact))
+router.post('/', asyncWrapper(addOneContact))
+router.put('/:contactId', asyncWrapper(updateOneContact))
+router.patch('/:contactId/favorite', asyncWrapper(updateOneStatus))
+router.delete('/:contactId', asyncWrapper(deleteOneContact))
 
 module.exports = router
