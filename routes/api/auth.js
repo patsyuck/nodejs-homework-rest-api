89a1e779b87e5d0a848ab8e4093e8a7e11f14719
Oleksii Patsiuk <patsyuck@ukr.net>
2021-09-05 09:12:29 +0300

    Додав заготовки для маршрутів та функцій автентифікації

---
+++ routes/api/auth.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const asyncWrapper = require('../middlewares/controllerWrapper')
+
+const register = async (req, res, next) => {}
+const login = async (req, res, next) => {}
+const logout = async (req, res, next) => {}
+const current = async (req, res, next) => {}
+
+router.post('/register', asyncWrapper(register))
+router.post('/login', asyncWrapper(login))
+router.post('/logout', asyncWrapper(logout))
+router.get('/current', asyncWrapper(current))
+
+module.exports = router
