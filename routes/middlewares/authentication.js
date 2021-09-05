2454c7d02df5209b024bd9067316c89f47352429
Oleksii Patsiuk <patsyuck@ukr.net>
2021-09-05 21:11:21 +0300

    Додав порівнняння токенів із заголовків і з БД

--- routes/middlewares/authentication.js
+++ routes/middlewares/authentication.js
@@ -13,6 +13,9 @@ const authentication = async (req, res, next) => {
     }
     const { id } = jwt.verify(token, SECRET_KEY)
     const user = await User.findById(id)
+    if (!user || (token !== user.token)) {
+      throw new createError(401, 'Not authorized')
+    }
     req.user = user
     next()
   } catch {
